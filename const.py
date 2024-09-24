0041c647b8031b89158342098cf854f75b01fb05
Daniel Wind <daniel@windit.nl>
2024-09-24 17:49:20 +0200

    fixes

--- const.py
+++ const.py
@@ -1,7 +1,9 @@
 """Constants for the Nightscout integration."""
 
-DOMAIN = "nightscout_custom"
+DOMAIN = "nightscout"
 
 ATTR_DEVICE = "device"
 ATTR_DELTA = "delta"
 ATTR_DIRECTION = "direction"
+MMOL_L = "mmol/L"
+MG_DL = "mg/dL"
